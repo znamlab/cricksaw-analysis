1f14bece7eea4204a02b352e4554dd8d0b59e67b
Antonin Blot <antonin.blot@gmail.com>
2022-01-31 14:35:32 +0000

    Add a script to run stitchit preprocessing

---
+++ scripts/stitchit_preprocessing.m
@@ -0,0 +1,9 @@
+% add relevant code folder
+addpath(genpath('/camp/lab/znamenskiyp/home/users/blota/code/yamlmatlab'));
+addpath(genpath('/camp/lab/znamenskiyp/home/users/blota/code/StitchIt/code'));
+
+% cd to relevant directory
+cd /camp/lab/znamenskiyp/data/instruments/raw_data/projects/rabies_barcoding/BRYC64.2h
+
+% run preProcessing
+preProcessTiles([], 'illumChans', (1:3)
