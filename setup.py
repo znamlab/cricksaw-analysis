cfc888b8cc87d4425d11a04732da465293cd677b
Antonin Blot <antonin.blot@gmail.com>
2022-08-05 09:53:07 +0100

    Add minimal requirements for elastix in setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name="cricksaw_analysis",
     packages=find_packages(),
+    install_requires=[
+        'SimpleITK-Elastix>=2.0.0rc2',
+        'numpy>=1.19.2',
+        'pyyaml>=6.0',
+        'pynrrd>=0.4.2',
+        'matplotlib>=3.5.1',
+    ],
 )
