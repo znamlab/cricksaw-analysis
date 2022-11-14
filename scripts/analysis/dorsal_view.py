2a8139d68f2bc8e8a2d878ab9bb1764c04c44aea
Antonin Blot <antonin.blot@crick.ac.uk>
2022-11-14 17:34:17 +0000

    Adapt dorsal_view scripts to multiple reg
    They can run with either elastix or brainreg registration

--- scripts/analysis/dorsal_view.py
+++ scripts/analysis/dorsal_view.py
@@ -6,8 +6,9 @@ Particularly useful to find blood vessel patterns
 
 import matplotlib
 import socket
-if socket.gethostname() == 'C02Z85AULVDC':
-    matplotlib.use('macosx')
+
+if socket.gethostname() == "C02Z85AULVDC":
+    matplotlib.use("macosx")
 
 from pathlib import Path
 import flexiznam as fzm
@@ -22,15 +23,42 @@ import tifffile
 
 from cricksaw_analysis import atlas_utils
 
-DATA_FOLDER = '/Users/blota/Data/PZAH5.6a/registration_10/PZAH5.6as_inverse_reg__elastix_out_step01/'
-imgs_path = dict(red=Path(DATA_FOLDER) / 'PZAH5.6as_inverse_reg__registration_step01.mhd',
-                 blue=None,
-                 green=None)
+PROCESSED = Path("/camp/lab/znamenskiyp/home/shared/projects")
+PROJECT = "hey2_3d-vision_foodres_20220101"
+MOUSE = "PZAG3.4f"
+REGISTRATION = "brainreg"
+ATLAS_SIZE = 10
 
-PATH_TO_SAVE = '/Users/blota/Data/dorsal_view/'
-ATLAS_NAME = 'allen_mouse_10um'
-ATLAS_ANNOTATION = '/Users/blota/Data/ARA_CCFv3/ARA_10_micron_mhd/atlas.mhd'
+if REGISTRATION == "brainreg":
+    DATA_FOLDER = PROCESSED / PROJECT / MOUSE / "brainreg_results"
+    imgs_path = dict(
+        red=Path(DATA_FOLDER) / "downsampled_standard.tiff",
+        blue=None,
+        green=Path(DATA_FOLDER) / "downsampled_standard_2.tiff",
+    )
+    ATLAS_ANNOTATION = None
+elif REGISTRATION == "elastix":
+    DATA_FOLDER = PROCESSED / PROJECT / MOUSE / "registration_10"
+    imgs_path = dict(
+        red=None,
+        blue=None,
+        green=Path(DATA_FOLDER)
+        / ("%ss_inverse_reg__elastix_out_step01" % MOUSE)
+        / ("%ss_inverse_reg__registration_step01.mhd" % MOUSE),
+    )
+    resource_folder = Path("/camp/lab/znamenskiyp/home/shared/resources")
+    atlas_resource = resource_folder / "cellfinder_resources/ARA_CCFv3"
+    ATLAS_ANNOTATION = atlas_resource / ("ARA_%d_micron_mhd/atlas.mhd" % ATLAS_SIZE)
+else:
+    raise IOError("Registration must be `brainreg` or `elastix`")
+
+
+PATH_TO_SAVE = DATA_FOLDER / "dorsal_view"
+ATLAS_NAME = "allen_mouse_%dum" % ATLAS_SIZE
 NAPARI = False
+
+if not PATH_TO_SAVE.is_dir():
+    PATH_TO_SAVE.mkdir()
 
 # do that first to crash early if there is a display issue
 if NAPARI:
@@ -40,24 +68,24 @@ PATH_TO_SAVE = Path(PATH_TO_SAVE)
 # make sure it exists
 PATH_TO_SAVE.mkdir(exist_ok=True)
 
-print('Loading atlas')
+print("Loading atlas")
 bg_atlas = bga.bg_atlas.BrainGlobeAtlas(ATLAS_NAME)
 # get registered data
-print('Loading img data')
+print("Loading img data")
 image_volumes = dict()
 for col, pa in imgs_path.items():
     if pa is None:
         continue
-    print('   loading %s channel' % col)
+    print("   loading %s channel" % col)
     image_volumes[col] = itk.array_from_image(itk.imread(str(pa)))
 
 # find layers
 ctx_df = atlas_utils.create_ctx_table(bg_atlas)
-layers = ['1', '2/3', '4', '5', '6a', '6b']
+layers = ["above", "1", "2/3", "4", "5", "6a", "6b"]
 if ATLAS_ANNOTATION is None:
-    atlas_annot =  bg_atlas.annotation.copy()
+    atlas_annot = bg_atlas.annotation.copy()
 else:
-   atlas_annot = itk.array_from_image(itk.imread(ATLAS_ANNOTATION))
+    atlas_annot = itk.array_from_image(itk.imread(ATLAS_ANNOTATION))
 peeled_atlas = np.array(atlas_annot, copy=True)
 
 atlas_dorsal_by_layer = dict()
@@ -69,28 +97,36 @@ atlas_index = dict()
 x, y = np.meshgrid(*[np.arange(bg_atlas.shape[i]) for i in [0, 2]])
 x = np.array(x, dtype=int).T
 y = np.array(y, dtype=int).T
-top_of_layer = atlas_utils.external_view(peeled_atlas, axis='dorsal',
-                                         border_only=False,
-                                         get_index=True, which='first')
+if layers[0] == "above":
+    top_of_layer = np.zeros(x.shape, dtype=int)
+else:
+    top_of_layer = atlas_utils.external_view(
+        peeled_atlas, axis="dorsal", border_only=False, get_index=True, which="first"
+    )
 for l in layers:
-    print('Doing %s' % l, flush=True)
+    print("Doing %s" % l, flush=True)
     atlas_index[l] = top_of_layer
-    layer_index = ctx_df.loc[ctx_df.layer == l, 'id'].values
     # make a dorsal view of the atlas
     atlas_layer = np.zeros(x.shape, dtype=atlas_annot.dtype)
     atlas_layer[x, y] = atlas_annot[x, top_of_layer, y]
     atlas_dorsal_by_layer[l] = atlas_layer
-    print('Peeling layer %s' % l, flush=True)
-    layer_mask = np.isin(atlas_annot, layer_index)
-    peeled_atlas[layer_mask] = 0
-    print('Find surface of next layer', flush=True)
-    bottom_of_layer = atlas_utils.external_view(peeled_atlas, axis='dorsal',
-                                                border_only=False,
-                                                get_index=True, which='first')
+
+    if l == "above":
+        # We don't change peeled_atlas, we will jsut look at the surface of registartion
+        pass
+    else:
+        layer_index = ctx_df.loc[ctx_df.layer == l, "id"].values
+        print("Peeling layer %s" % l, flush=True)
+        layer_mask = np.isin(atlas_annot, layer_index)
+        peeled_atlas[layer_mask] = 0
+
+    print("Find surface of next layer", flush=True)
+    bottom_of_layer = atlas_utils.external_view(
+        peeled_atlas, axis="dorsal", border_only=False, get_index=True, which="first"
+    )
 
     thickness = bottom_of_layer - top_of_layer
     max_diff = np.max(thickness)
-
 
     for color, img_volume in image_volumes.items():
         data_view = np.zeros(x.shape, dtype=img_volume.dtype)
@@ -99,31 +135,51 @@ for l in layers:
         for i in range(max_diff):
             mask = thickness >= i
             # for max proj
-            data_view[x[mask], y[mask]] = np.maximum(data_view[x[mask], y[mask]],
-                                                     img_volume[x[mask],
-                                                                top_of_layer[mask] + i,
-                                                                y[mask]]
-                                                     )
-        #TODO: From here the script has not been adapted to 3 colors
-        data_dorsal_by_layer[l] = data_view
-        # new top and iterate
-        top_of_layer = bottom_of_layer
-    atlas_index['wm'] = bottom_of_layer
+            data_view[x[mask], y[mask]] = np.maximum(
+                data_view[x[mask], y[mask]],
+                img_volume[
+                    x[mask],
+                    np.clip(top_of_layer[mask] + i, 0, img_volume.shape[1] - 1),
+                    y[mask],
+                ],
+            )
+        if l not in data_dorsal_by_layer:
+            data_dorsal_by_layer[l] = dict()
+        data_dorsal_by_layer[l][color] = data_view
+        # TODO: From here the script has not been adapted to 3 colors
+
+    # new top and iterate
+    top_of_layer = bottom_of_layer
+    atlas_index["wm"] = bottom_of_layer
 
 if NAPARI:
-    print('Adding to napari', flush=True)
+    print("Adding to napari", flush=True)
     for l in layers[::-1]:
-        viewer.add_image(data=bg_dorsal_by_layer[l], name='Background layer %s' % l,
-                         colormap='green', contrast_limits=[0, 500], blending='opaque',
-                         visible=False)
-        viewer.add_image(data=data_dorsal_by_layer[l], name='Signal layer %s' % l,
-                         colormap='gray', contrast_limits=[0, 5000], blending='additive')
+        viewer.add_image(
+            data=bg_dorsal_by_layer[l],
+            name="Background layer %s" % l,
+            colormap="green",
+            contrast_limits=[0, 500],
+            blending="opaque",
+            visible=False,
+        )
+        viewer.add_image(
+            data=data_dorsal_by_layer[l],
+            name="Signal layer %s" % l,
+            colormap="gray",
+            contrast_limits=[0, 5000],
+            blending="additive",
+        )
 
-        viewer.add_labels(data=atlas_dorsal_by_layer[l], name='atlas %s' % l,
-                          opacity=0.1, visible=True if l == '1' else False)
+        viewer.add_labels(
+            data=atlas_dorsal_by_layer[l],
+            name="atlas %s" % l,
+            opacity=0.1,
+            visible=True if l == "1" else False,
+        )
 
 if PATH_TO_SAVE is not None:
-    print('Saving in %s' % PATH_TO_SAVE, flush=True)
+    print("Saving in %s" % PATH_TO_SAVE, flush=True)
     save_root = Path(PATH_TO_SAVE)
     save_root.mkdir(exist_ok=True)
     assert save_root.is_dir()
@@ -133,51 +189,56 @@ if PATH_TO_SAVE is not None:
     fig.subplots_adjust(top=0.95, right=0.99, bottom=0.05, left=0.07)
     midline = int(atlas_dorsal_by_layer[l].shape[1] / 2)
     for l in layers:
-        ax.clear()
-        ax.set_aspect('equal')
-        atlas_utils.plot_borders_and_areas(ax, atlas_dorsal_by_layer[l],
-                                           areas_to_plot=[])
-        # do a version with label on right hemisphere
-        right_hem = np.zeros_like(atlas_dorsal_by_layer[l])
-        right_hem[:, midline:] = atlas_dorsal_by_layer[l][:,midline:]
-        atlas_utils.plot_borders_and_areas(ax, right_hem,
-                                           areas_to_plot=[],
-                                           label_atlas=bg_atlas)
-        img = ax.imshow(data_dorsal_by_layer[l], cmap='Greys_r')
-        ax.set_title('Layer %s' % l)
-        layer_name = l.replace('/', '_')
-        fig.savefig(save_root / ('dorsal_view_layer_%s.png' % layer_name), dpi=600)
-        fig.savefig(save_root / ('dorsal_view_layer_%s.svg' % layer_name), dpi=600)
-        if REGISTERED_BACKGROUND_DATA is not None:
-            img.remove()
-            ax.set_title('Layer %s - background' % l)
-            img = ax.imshow(bg_dorsal_by_layer[l], cmap='Greys_r')
-            fig.savefig(save_root / ('dorsal_view_layer_background_%s.png' % layer_name),
-                        dpi=600)
-            fig.savefig(save_root / ('dorsal_view_layer_background_%s.svg' % layer_name),
-                        dpi=600)
-            img.remove()
-            rgb = np.zeros(list(bg_dorsal_by_layer[l].shape) + [3],
-                           dtype=np.uint8)
-            top = np.quantile(bg_dorsal_by_layer[l], 0.99)
-            bg = bg_dorsal_by_layer[l] / top * 255
-            bg[bg > 255] = 255
-            bg = np.array(bg, dtype=np.uint8)
-            rgb[:, :, 0] = bg
-            rgb[:, :, 2] = bg
-            top = np.quantile(data_dorsal_by_layer[l], 0.99)
-            dt = data_dorsal_by_layer[l]/ top * 255
-            dt[dt > 255] = 255
-            dt = np.array(dt, dtype=np.uint8)
+        # decide which layer of the atlas we will plot
+        atlas_layer = l if l != "above" else "1"
+        for color in image_volumes.keys():
+            data_this_color = data_dorsal_by_layer[l][color]
+            ax.clear()
+            ax.set_aspect("equal")
+            atlas_utils.plot_borders_and_areas(
+                ax, atlas_dorsal_by_layer[atlas_layer], areas_to_plot=[]
+            )
+            # do a version with label on right hemisphere
+            right_hem = np.zeros_like(atlas_dorsal_by_layer[atlas_layer])
+            right_hem[:, midline:] = atlas_dorsal_by_layer[atlas_layer][:, midline:]
+            atlas_utils.plot_borders_and_areas(
+                ax, right_hem, areas_to_plot=[], label_atlas=bg_atlas
+            )
+            img = ax.imshow(data_this_color, cmap="Greys_r")
+            ax.set_title("Layer %s" % l)
+            layer_name = l.replace("/", "_")
+            fig.savefig(
+                save_root / ("dorsal_view_layer_%s_%s.png" % (layer_name, color)),
+                dpi=600,
+            )
+            fig.savefig(
+                save_root / ("dorsal_view_layer_%s_%s.svg" % (layer_name, color)),
+                dpi=600,
+            )
 
-            rgb[:, :, 1] = dt
+        if len(data_dorsal_by_layer[l]) > 1:
+            print("Making RGB image for layer %s" % l)
+            img.remove()
+            rgb = np.zeros(list(atlas_dorsal_by_layer[l].shape) + [3], dtype=np.uint8)
+            colord = ["red", "green", "blue"]
+            for color, data in data_dorsal_by_layer[l].items():
+                if color not in colord:
+                    raise IOError("unknown color: %s" % color)
+                colind = colord.index(color)
+                top = np.quantile(data, 0.99)
+                normed = np.array(data) / top * 255
+                normed[normed > 255] = 255
+                normed = np.array(normed, dtype=np.uint8)
+                rgb[:, :, colind] = normed
             img = ax.imshow(rgb)
-            ax.set_title('Layer %s - Magenta background, Green data' % l)
-            fig.savefig(save_root / ('dorsal_view_layer_rgb_%s.png' % layer_name),
-                        dpi=600)
-            fig.savefig(save_root / ('dorsal_view_layer_rgb_%s.svg' % layer_name),
-                        dpi=600)
+            ax.set_title("Layer %s - RGB " % l)
+            fig.savefig(
+                save_root / ("dorsal_view_layer_rgb_%s.png" % layer_name), dpi=600
+            )
+            fig.savefig(
+                save_root / ("dorsal_view_layer_rgb_%s.svg" % layer_name), dpi=600
+            )
 else:
-    print('Do not save', flush=True)
+    print("Do not save", flush=True)
 
-print('done')
+print("done")
--- scripts/analysis/dorsal_view_stack.py
+++ scripts/analysis/dorsal_view_stack.py
@@ -6,8 +6,9 @@ Particularly useful to find blood vessel patterns
 
 import matplotlib
 import socket
-if socket.gethostname() == 'C02Z85AULVDC':
-    matplotlib.use('macosx')
+
+if socket.gethostname() == "C02Z85AULVDC":
+    matplotlib.use("macosx")
 
 from pathlib import Path
 import bg_atlasapi as bga
@@ -16,15 +17,43 @@ import numpy as np
 from napari.viewer import Viewer
 from cricksaw_analysis import atlas_utils
 
-DATA_FOLDER = '/Users/blota/Data/PZAH5.6a/registration_10/PZAH5.6as_inverse_reg__elastix_out_step01/'
-imgs_path = dict(red=Path(DATA_FOLDER) / 'PZAH5.6as_inverse_reg__registration_step01.mhd',
-                 blue=None,
-                 green=None)
 
-PATH_TO_SAVE = '/Users/blota/Data/dorsal_view/'
-ATLAS_NAME = 'allen_mouse_10um'
-ATLAS_ANNOTATION = '/Users/blota/Data/ARA_CCFv3/ARA_10_micron_mhd/atlas.mhd'
+PROCESSED = Path("/camp/lab/znamenskiyp/home/shared/projects")
+PROJECT = "hey2_3d-vision_foodres_20220101"
+MOUSE = "PZAH6.4b"
+REGISTRATION = "brainreg"
+ATLAS_SIZE = 10
+
+if REGISTRATION == "brainreg":
+    DATA_FOLDER = PROCESSED / PROJECT / MOUSE / "brainreg_results"
+    imgs_path = dict(
+        red=Path(DATA_FOLDER) / "downsampled_standard.tiff",
+        blue=None,
+        green=Path(DATA_FOLDER) / "downsampled_standard_2.tiff",
+    )
+    ATLAS_ANNOTATION = None  # will load from brainglobe
+elif REGISTRATION == "elastix":
+    DATA_FOLDER = PROCESSED / PROJECT / MOUSE / "registration_10"
+    imgs_path = dict(
+        red=None,
+        blue=None,
+        green=Path(DATA_FOLDER)
+        / ("%ss_inverse_reg__elastix_out_step01" % MOUSE)
+        / ("%ss_inverse_reg__registration_step01.mhd" % MOUSE),
+    )
+    resource_folder = Path("/camp/lab/znamenskiyp/home/shared/resources")
+    atlas_resource = resource_folder / "cellfinder_resources/ARA_CCFv3"
+    ATLAS_ANNOTATION = atlas_resource / ("ARA_%d_micron_mhd/atlas.mhd" % ATLAS_SIZE)
+else:
+    raise IOError("Registration must be `brainreg` or `elastix`")
+
+PATH_TO_SAVE = DATA_FOLDER / "dorsal_view"
+PATH_TO_SAVE /= "stacks"
+ATLAS_NAME = "allen_mouse_%dum" % ATLAS_SIZE
 NAPARI = False
+
+if not PATH_TO_SAVE.is_dir():
+    PATH_TO_SAVE.mkdir()
 
 # do that first to crash early if there is a display issue
 if NAPARI:
@@ -34,24 +63,24 @@ PATH_TO_SAVE = Path(PATH_TO_SAVE)
 # make sure it exists
 PATH_TO_SAVE.mkdir(exist_ok=True)
 
-print('Loading atlas')
+print("Loading atlas")
 bg_atlas = bga.bg_atlas.BrainGlobeAtlas(ATLAS_NAME)
 # get registered data
-print('Loading img data')
+print("Loading img data")
 image_volumes = dict()
 for col, pa in imgs_path.items():
     if pa is None:
         continue
-    print('   loading %s channel' % col)
+    print("   loading %s channel" % col)
     image_volumes[col] = itk.array_from_image(itk.imread(str(pa)))
 
 # find layers
 ctx_df = atlas_utils.create_ctx_table(bg_atlas)
-layers = ['1', '2/3', '4', '5', '6a', '6b']
+layers = ["1", "2/3", "4", "5", "6a", "6b"]
 if ATLAS_ANNOTATION is None:
-    atlas_annot =  bg_atlas.annotation.copy()
+    atlas_annot = bg_atlas.annotation.copy()
 else:
-   atlas_annot = itk.array_from_image(itk.imread(ATLAS_ANNOTATION))
+    atlas_annot = itk.array_from_image(itk.imread(ATLAS_ANNOTATION))
 peeled_atlas = np.array(atlas_annot, copy=True)
 
 atlas_dorsal_by_layer = dict()
@@ -63,24 +92,24 @@ atlas_index = dict()
 x, y = np.meshgrid(*[np.arange(bg_atlas.shape[i]) for i in [0, 2]])
 x = np.array(x, dtype=int).T
 y = np.array(y, dtype=int).T
-top_of_layer = atlas_utils.external_view(peeled_atlas, axis='dorsal',
-                                         border_only=False,
-                                         get_index=True, which='first')
+top_of_layer = atlas_utils.external_view(
+    peeled_atlas, axis="dorsal", border_only=False, get_index=True, which="first"
+)
 for l in layers:
-    print('Doing %s' % l, flush=True)
+    print("Doing %s" % l, flush=True)
     atlas_index[l] = top_of_layer
-    layer_index = ctx_df.loc[ctx_df.layer == l, 'id'].values
+    layer_index = ctx_df.loc[ctx_df.layer == l, "id"].values
     # make a dorsal view of the atlas
     atlas_layer = np.zeros(x.shape, dtype=atlas_annot.dtype)
     atlas_layer[x, y] = atlas_annot[x, top_of_layer, y]
     atlas_dorsal_by_layer[l] = atlas_layer
-    print('Peeling layer %s' % l, flush=True)
+    print("Peeling layer %s" % l, flush=True)
     layer_mask = np.isin(atlas_annot, layer_index)
     peeled_atlas[layer_mask] = 0
-    print('Find surface of next layer', flush=True)
-    bottom_of_layer = atlas_utils.external_view(peeled_atlas, axis='dorsal',
-                                                border_only=False,
-                                                get_index=True, which='first')
+    print("Find surface of next layer", flush=True)
+    bottom_of_layer = atlas_utils.external_view(
+        peeled_atlas, axis="dorsal", border_only=False, get_index=True, which="first"
+    )
 
     thickness = bottom_of_layer - top_of_layer
     max_diff = np.max(thickness)
@@ -93,7 +122,13 @@ for l in layers:
         for name, i in enumerate(range(-10, 30)):
             img = img_volume[x, top_of_layer + i, y]
             img = (img - zero) * (65536 / extent.max())
-            img = np.array(img , dtype='uint16')
-            itk.imwrite(itk.image_from_array(img),
-                        PATH_TO_SAVE / ('dorsal_view_%02d.png' % name))
+            img = np.array(img, dtype="uint16")
+            itk.imwrite(
+                itk.image_from_array(img),
+                PATH_TO_SAVE
+                / (
+                    "dorsal_view_stack_around_layer_%s_%s_%02d.png"
+                    % (l.replace("/", ""), color, name)
+                ),
+            )
         # do the extended depth of focus in FIJI
