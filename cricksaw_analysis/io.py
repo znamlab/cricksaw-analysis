907beb99ce73cfa30414c90d21d2319c7142b5f6
Antonin Blot <antonin.blot@crick.ac.uk>
2023-01-31 14:38:19 +0000

    Format with black

--- cricksaw_analysis/io.py
+++ cricksaw_analysis/io.py
@@ -21,22 +21,22 @@ def load_cellfinder_results(cellfinder_folder):
     """
     cellfinder_folder = Path(cellfinder_folder)
     if not cellfinder_folder.is_dir():
-        raise IOError('%s is not a directory' % cellfinder_folder)
-    reg_folder = cellfinder_folder / 'registration'
+        raise IOError("%s is not a directory" % cellfinder_folder)
+    reg_folder = cellfinder_folder / "registration"
     if not reg_folder.is_dir():
-        raise IOError('Registration folder not found')
-    pts_folder = cellfinder_folder / 'points'
+        raise IOError("Registration folder not found")
+    pts_folder = cellfinder_folder / "points"
     if not pts_folder.is_dir():
-        raise IOError('Points folder not found')
+        raise IOError("Points folder not found")
 
     downsampled_stacks = {}
-    for fname in reg_folder.glob('downsampled*.tiff'):
-        if 'standard' in fname.stem:
+    for fname in reg_folder.glob("downsampled*.tiff"):
+        if "standard" in fname.stem:
             continue
         downsampled_stacks[fname.stem] = tifffile.imread(fname)
 
-    atlas = tifffile.imread(reg_folder / 'registered_atlas.tiff')
+    atlas = tifffile.imread(reg_folder / "registered_atlas.tiff")
 
-    pts = pd.read_hdf(pts_folder / 'downsampled.points')
+    pts = pd.read_hdf(pts_folder / "downsampled.points")
 
     return pts, downsampled_stacks, atlas
